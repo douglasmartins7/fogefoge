cfa6ed67810b5f80525cabdf892e6e2460e45b4b
Douglas Martins Santos <douglassantos@cvc.com.br>
2017-12-05 21:13:32 -0200

    fantasmas andam para a direita e não atravessam o muro

--- fogefoge.rb
+++ fogefoge.rb
@@ -8,7 +8,7 @@ def le_mapa(numero)
 end
 
 def joga(nome)
-    mapa = le_mapa(1)
+    mapa = le_mapa(2)
     while true
         desenha mapa
         direcao = pede_movimento
@@ -21,6 +21,8 @@ def joga(nome)
 
         mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+        move_fantasmas mapa
+        #move_fantasma mapa, linha, coluna
     end
 end
 
@@ -55,17 +57,16 @@ end
 
 def calcula_nova_posicao(heroi, direcao)
     heroi = heroi.dup
-    case direcao
-        when "W"
-            heroi[0] -= 1
-        when "S"
-            heroi[0] += 1
-        when "A"
-            heroi[1] -= 1
-        when "D"
-            heroi[1] += 1
-        end
-        heroi
+    movimentos = {
+        "W" => [-1, 0],
+        "S" => [+1, 0],
+        "A" => [0, -1],
+        "D" => [0, +1]
+        }
+    movimento = movimentos[direcao]
+    heroi[0] += movimento[0]
+    heroi[1] += movimento[1]
+    heroi
 end
 
 
@@ -80,9 +81,29 @@ def posicao_valida?(mapa, posicao)
         return false
     end
 
-    if mapa[posicao[0]][posicao[1]] == "X"
+    valor_local = mapa[posicao[0]][posicao[1]]
+    if valor_local == "X" || valor_local == "F"
         return false
     end
-
     true
+end
+
+def move_fantasmas(mapa)
+    caracter_do_fantasma = "F"
+    mapa.each_with_index do |linha_atual, linha|
+        linha_atual.chars.each_with_index do |caracter_atual, coluna|
+            eh_fantasma = caracter_atual == caracter_do_fantasma
+            if eh_fantasma
+                move_fantasma mapa, linha, coluna
+            end
+        end
+    end
+end
+
+def move_fantasma(mapa, linha, coluna)
+    posicao = [linha, coluna + 1]
+    if posicao_valida? mapa, posicao
+        mapa[linha][coluna] = " "
+        mapa[posicao[0]][posicao[1]] = "F"
+    end
 end
